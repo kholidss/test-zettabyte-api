5dfeb3f64cbd46fc096c8cea173b43637dfd0c41
kholid saifulloh <muhammadkholid@refactory.id>
2021-11-11 09:50:37 +0700

    edit result in health controller

--- src/controllers/health.controller.js
+++ src/controllers/health.controller.js
@@ -4,7 +4,8 @@ const healthCheck = async (req, res) => {
   try {
     const result = await healthService.healthCheck();
     const dateNow = new Date(Date.now()).toISOString();
-    return res.status(200).json({ message: dateNow, time: dateNow });
+
+    return res.status(200).json({ message: result, time: dateNow });
   } catch (error) {
     console.log(error);
   }
