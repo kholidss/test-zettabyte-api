bba5f726107488b844c58db773061d85b93f426d
kholid saifulloh <muhammadkholid@refactory.id>
2021-11-13 12:25:23 +0700

    Add register write file

--- src/services/register.services.js
+++ src/services/register.services.js
@@ -1,8 +1,17 @@
+import fs from 'fs'
+import YAML from 'yaml'
+import mkdirp from 'mkdirp'
 import { UserModel } from '../models/user.model'
+import { randomString } from '../utils/helpers/random-string.helper'
 
 const register = async (bodyRegister) => {
   const newUser = new UserModel(bodyRegister)
   const save = await newUser.save()
+
+  // eslint-disable-next-line no-underscore-dangle
+  const madeDir = mkdirp.sync(`data/${save._id}`)
+  const registerDataYAML = YAML.stringify({EMAIL: save.email, NAME: save.name})
+  fs.writeFileSync(`data/${save._id}/${randomString(10)}.yml`, registerDataYAML)
 
   return {
     // eslint-disable-next-line no-underscore-dangle
